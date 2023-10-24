73e42e17c0d8150ea4169f7c799f4cf95f4345b7
luoxy <3366018479@qq.com>
2023-10-24 17:20:00 +0800

    feat: 添加类型信息章节的代码.

---
+++ src/main/java/chapter14Typeinfo/SimpleDynamicProxy.java
@@ -0,0 +1,51 @@
+package chapter14Typeinfo;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+/**
+ * @Date: 2023/10/24 13:51
+ * @Author: Sean Luo
+ * @Description:
+ */
+class DynamicProxyHandle implements InvocationHandler {
+
+    private Object proxied;
+
+    public DynamicProxyHandle(Object proxied) {
+        this.proxied = proxied;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("**** proxy: " + proxy.getClass() +
+                ", method: " + method + ", args: " + Arrays.toString(args));
+        if (args != null) {
+            for (Object arg : args) {
+                System.out.println(" " + arg);
+            }
+        }
+        return method.invoke(proxied, args);
+    }
+}
+
+class SimpleDynamicProxy {
+
+    public static void consumer(Interface iface) {
+        iface.doSomething();
+        iface.somethingElse("bonobo");
+    }
+
+    public static void main(String[] args) {
+        RealObject real = new RealObject();
+        consumer(real);
+        // Insert a proxy and call again
+        Interface proxy = (Interface) Proxy.newProxyInstance(
+                Interface.class.getClassLoader(),
+                new Class[] {Interface.class},
+                new DynamicProxyHandle(real));
+        consumer(proxy);
+    }
+}
