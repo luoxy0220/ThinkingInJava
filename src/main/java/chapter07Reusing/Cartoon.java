25df23b8284ee88cc04ca93cae415b9e165b0280
luoxy <3366018479@qq.com>
2023-10-31 17:53:28 +0800

    feat: 添加复用类章节的代码.

---
+++ src/main/java/chapter07Reusing/Cartoon.java
@@ -0,0 +1,17 @@
+package chapter07Reusing;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * @Date: 2023/10/31 17:08
+ * @Author: Sean Luo
+ * @Description: Constructor calls during inheritance
+ */
+class Art {
+
+    Art() {
+        print("Art constructor");
+    }
+}
+public class Cartoon {
+}
---
+++ src/main/java/chapter07Reusing/SprinklerSystem.java
@@ -0,0 +1,44 @@
+package chapter07Reusing;
+
+/**
+ * @Date: 2023/10/31 14:21
+ * @Author: Sean Luo
+ * @Description: Composition for code reuse.
+ */
+class WaterSource {
+
+    private String s;
+
+    WaterSource() {
+        System.out.println("WaterSource()");
+        s = "Constructed";
+    }
+
+    public String toString() {
+        return s;
+    }
+}
+public class SprinklerSystem {
+
+    private String valve1, valve2, valve3, valve4;
+
+    private WaterSource source = new WaterSource();
+
+    private int i;
+    private float f;
+
+    public String toString() {
+        return
+                "valve1 = " + valve1 + " " +
+                "valve2 = " + valve2 + " " +
+                "valve3 = " + valve3 + " " +
+                "valve4 = " + valve4 + "\n" +
+                "i = " + i + " " + "f = " + f + " " +
+                "source = " + source;
+    }
+
+    public static void main(String[] args) {
+        SprinklerSystem sprinklers = new SprinklerSystem();
+        System.out.println(sprinklers);
+    }
+}
