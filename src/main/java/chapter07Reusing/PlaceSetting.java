f41221200b45fa3bf13514605659022fa576293f
luoxy <3366018479@qq.com>
2023-11-01 17:00:31 +0800

    feat: 添加复用类章节的代码.

--- src/main/java/chapter07Reusing/PlaceSetting.java
+++ src/main/java/chapter07Reusing/PlaceSetting.java
@@ -54,5 +54,22 @@ class Custom {
     }
 }
 
-public class PlaceSetting {
+public class PlaceSetting extends Custom {
+    private Spoon sp;
+    private Fork frk;
+    private Knife kn;
+    private DinnerPlate pl;
+
+    public PlaceSetting(int i) {
+        super(i + 1);
+        sp = new Spoon(i + 2);
+        frk = new Fork(i + 3);
+        kn = new Knife(i + 4);
+        pl = new DinnerPlate(i + 5);
+        print("PlaceSetting constructor");
+    }
+
+    public static void main(String[] args) {
+        PlaceSetting x = new PlaceSetting(9);
+    }
 }
