6779cfc974b11c3a84598e12a52419dc4aa79f34
luoxy <3366018479@qq.com>
2023-11-02 17:29:07 +0800

    feat: 添加复用类章节的代码.

---
+++ src/main/java/chapter07Reusing/BlankFinal.java
@@ -0,0 +1,36 @@
+package chapter07Reusing;
+
+/**
+ * @Date: 2023/11/2 16:40
+ * @Author: Sean Luo
+ * @Description: "Blank" final fields
+ */
+class Poppet {
+    private int i;
+
+    Poppet(int i1) {
+        i = i1;
+    }
+}
+
+public class BlankFinal {
+    private final int i = 0;  // Initialized final
+    private final int j;  // Blank final
+    private final Poppet p;  // Blank final reference
+
+    // Blank finals MUST be initialized in the constructor
+    public BlankFinal() {
+        j = 1;  // Initialized blank final
+        p = new Poppet(1);  // Initialized blank final reference
+    }
+
+    public BlankFinal(int x) {
+        j = x;  // Initialized blank final
+        p = new Poppet(x);  // Initialized blank final reference
+    }
+
+    public static void main(String[] args) {
+        new BlankFinal();
+        new BlankFinal(47);
+    }
+}
---
+++ src/main/java/chapter07Reusing/Wind.java
@@ -0,0 +1,23 @@
+package chapter07Reusing;
+
+/**
+ * @Date: 2023/11/2 14:05
+ * @Author: Sean Luo
+ * @Description: Inheritance & upcasting
+ */
+class Instrument {
+    public void play() {}
+
+    static void tune(Instrument i) {
+        i.play();
+    }
+}
+
+// Wind objects are instruments
+// because they have the same interface
+public class Wind extends Instrument {
+    public static void main(String[] args) {
+        Wind flute = new Wind();
+        Instrument.tune(flute);
+    }
+}
